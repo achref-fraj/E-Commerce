6e09223cc5343987fd44bdeac28372c2440225d3
achref-fraj <101942901+achref-fraj@users.noreply.github.com>
2025-01-15 07:33:42 -1100

    commit

--- front/src/components/Category.js
+++ front/src/components/Category.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 function Category() {
+  const [dataCateg, setCateg] = useState([])
+
+  console.log("dataCateg", dataCateg)
+
+  useEffect(() => {
+    fetchCateg()
+  }, [])
+
+  const fetchCateg = () => {
+    axios.get("http://localhost:3000/api/category/")
+      .then((res) => {
+        setCateg(res.data)
+      })
+      .catch((err) => {
+        console.log("fama error gad rasek", err)
+      })
+  }
+
   return (
-    <div>Category :</div>
+    <div>
+      <h4>Category:</h4>
+      <select>
+        {dataCateg.map((ele, i) => (
+          <option key={i} value={ele.Name}>
+            {ele.Name}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
 
